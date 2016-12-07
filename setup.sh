7b720bca9284b55cda6662f347bca16daafafc87
Andrew Welleck <andrew8838@gmail.com>
2016-12-06 22:39:03 -0600

    Fixing backup directory creation

--- setup.sh
+++ setup.sh
@@ -22,6 +22,17 @@ if [ -d $backup_dir ]; then
 		if [ ! -d $backup_dir$a ]; then
 			echo "Creating $backup_dir$a"
 			mkdir $backup_dir$a
+
+			for (( b=0; b<5; b++ ))
+			do
+				if [ -f ${FILES[$b]} ] && [-d $backup_dir$a ]; then
+					echo "THe following file exists: ${FILES[$b]}"
+					mv ${FILES[$b]} $backup_dir$a
+				elif [ ! -f ${FILES[$b]} ]; then
+					echo "The following file does not exist: ${FILES[$b]}"
+				fi
+			done
+
 			break
 		elif [ -d $backup_dir$a ]; then
 			continue
@@ -30,6 +41,16 @@ if [ -d $backup_dir ]; then
 elif [ ! -d $backup_dir ]; then
 	echo "Creating $backup_dir"
 	mkdir $backup_dir
+
+	for (( b=0; b<5; b++ ))
+	do
+		if [ -f ${FILES[$b]} ] && [-d $backup_dir ]; then
+			echo "The following file exists: ${FILES[$b]}"
+			mv ${FILES[$b]} $backup_dir
+		elif [ ! -f ${FILES[$b]} ]; then
+			echo "The following file does not exist: ${FILES[$b]}"
+		fi
+	done
 fi
 
 #check if each of the files in the array exist
@@ -46,28 +67,17 @@ do
 	fi
 done
 
-current_dir=$(pwd)
-#cd $HOME/ && curl -sS -O
-#cd $HOME/ && curl -sS -O
-#cd $HOME/.vim/colors && curl -sS -O
-#cd $HOME/ && curl -sS -O
-cd $current_dir
-
 #Check if ~/.vim/colors exists, if not create it
-#if [ -d $vim_colors ]; then
-#	echo "The ~/.vim/colors directory alreay exists."
-#else
-#	echo "Creating .vim/colors directory..."
-#	mkdir "$HOME/.vim/colors"
-#fi
-#
-##Check if a .vimrc file exits, if not curl from GitHub
-#if [ -f $vimrc_file ]; then
-#	echo "A .vimrc file already exists."
-#	mkdir "$HOME/old_configs"
-#	cp $vimrc_file $HOME/old_configs
-#	curl -o .vimrc https://raw.githubusercontent.com/awelleck/dotfiles/master/.vimrc
-#else
-#	echo "Using curl to download .vimrc file..."
-#	curl -o .vimrc https://raw.githubusercontent.com/awelleck/dotfiles/master/.vimrc
-#fi
+if [ -d $vim_colors ]; then
+	echo "The ~/.vim/colors directory alreay exists."
+else
+	echo "Creating .vim/colors directory..."
+	mkdir "$HOME/.vim/colors"
+fi
+
+current_dir=$(pwd)
+cd $HOME/ && curl -sS -O https://raw.githubusercontent.com/awelleck/dotfiles/master/.gitconfig
+cd $HOME/ && curl -sS -O https://raw.githubusercontent.com/awelleck/dotfiles/master/.vimrc
+cd $HOME/.vim/colors && curl -sS -O https://raw.githubusercontent.com/encody/nvim/master/colors/lyla.vim
+cd $current_dir
+echo "Done!"
