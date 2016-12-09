02ccbda284d156f71a771abe1696b0271fd30439
Andrew Welleck <andrew8838@gmail.com>
2016-12-09 17:47:45 -0600

    Added .bash_profile and cleaned up setup.sh

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+# If the .bashrc file exists, source
+if [ -f ~/.bashrc ]; then
+	source ~/.bashrc
+fi
+
+export EDITOR=/usr/bin/vim 			#sets default editor
+export CLICOLOR=1 					#enable colors in terminal
+LSCOLORS=exfxcxdxbxegedabagacad 	#color scheme
+alias qfind="find . -name " 		#qfind: Quickly search for file
--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #Automate installation of dotfiles
 backup_dir=$HOME/dotfiles_backup
 vimrc_file=$HOME/.vimrc
-vim_colors=$HOME/.vim/colors/lyla.vim
+vim_colors=$HOME/.vim/colors/wombat256dave.vim
 vim_colors_dir=$HOME/.vim/colors
-bash_profile=$HOME/.bash_profile
-alias_file=$HOME/.alias
 gitconfig=$HOME/.gitconfig
+bash_profile=$HOME/.bash_profile
+
 
 FILES[0]=$vimrc_file
 FILES[1]=$vim_colors
-FILES[2]=$bash_profile
-FILES[3]=$alias_file
-FILES[4]=$gitconfig
+FILES[2]=$gitconfig
+FILES[3]=$bash_profile
 
 echo "Setup script for dotfiles:"
+echo "=========================="
 
-#check if /dotfile_backup exists, create if not
+#Check if /dotfile_backup exists, create if not
 if [ -d $backup_dir ]; then
 	echo "The /dotfile_backup dir exists"
 	for (( a=1; ; a++ ))
@@ -24,7 +24,7 @@ if [ -d $backup_dir ]; then
 			echo "Creating $backup_dir$a"
 			mkdir $backup_dir$a
 
-			for (( b=0; b<5; b++ ))
+			for (( b=0; b<4; b++ ))
 			do
 				if [ -f ${FILES[$b]} ] && [ -d $backup_dir$a ]; then
 					echo "The following file exists: ${FILES[$b]}"
@@ -43,7 +43,7 @@ elif [ ! -d $backup_dir ]; then
 	echo "Creating $backup_dir"
 	mkdir $backup_dir
 
-	for (( b=0; b<5; b++ ))
+	for (( b=0; b<4; b++ ))
 	do
 		if [ -f ${FILES[$b]} ] && [ -d $backup_dir ]; then
 			echo "The following file exists: ${FILES[$b]}"
@@ -54,32 +54,18 @@ elif [ ! -d $backup_dir ]; then
 	done
 fi
 
-#check if each of the files in the array exist
-#for (( b=0; b<5; b++ ))
-#do
-#	if [ -f ${FILES[$b]} ] && [ -d $backup_dir ]; then
-#		echo "The following file exists: ${FILES[$b]}"
-#		mv ${FILES[$b]} $backup_dir
-#	elif [ -f ${FILES[$b]} ] && [ -d $backup_dir$a ]; then
-#		echo "THe following file exists: ${FILES[$b]}"
-#		mv ${FILES[$b]} $backup_dir$a
-#	elif [ ! -f ${FILES[$b]} ]; then
-#		echo "The following file does not exist: ${FILES[$b]}"
-#	fi
-#done
-
 #Check if ~/.vim/colors exists, if not create it
 if [ -d $vim_colors_dir ]; then
-	echo "The ~/.vim/colors directory alreay exists."
+	echo "The ~/.vim/colors directory alreay exists"
 elif [ ! -d $vim_colors_dir ]; then
-	echo "Creating .vim/colors directory..."
-	mkdir $HOME/.vim
-	mkdir $HOME/.vim/colors
+	echo "Creating .vim/colors directory"
+	mkdir -p $HOME/.vim/colors
 fi
 
 current_dir=$(pwd)
-cd $HOME/ && curl -sS -O https://raw.githubusercontent.com/awelleck/dotfiles/master/.gitconfig
 cd $HOME/ && curl -sS -O https://raw.githubusercontent.com/awelleck/dotfiles/master/.vimrc
-cd $HOME/.vim/colors && curl -sS -O https://raw.githubusercontent.com/encody/nvim/master/colors/lyla.vim
+cd $HOME/.vim/colors && curl -sS -O https://raw.githubusercontent.com/davb5/wombat256dave/master/colors/wombat256dave.vim
+cd $HOME/ && curl -sS -O https://raw.githubusercontent.com/awelleck/dotfiles/master/.gitconfig
+cd $HOME/ && curl -sS -O https://raw.githubusercontent.com/awelleck/dotfiles/master/.bash_profile
 cd $current_dir
 echo "Done!"
